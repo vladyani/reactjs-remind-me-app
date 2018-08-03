a77014ca21e5e8942118e183f80cb3beb5d5fede
Vlad Yantsov <vladyslavyantsov@gmail.com>
2018-08-03 19:35:47 +0100

    project build tools added

---
+++ js/components/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import RemindMe from './RemindMe/RemindMe.jsx'
+
+export default class App extends Component {
+ 
+    
+    render() {
+        
+        return (
+            <RemindMe />
+        )
+  }
+}
